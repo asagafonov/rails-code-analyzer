ae204702e169840e72e91a012393ec303e0a0786
asagafonov <agafonov@buroburo.ru>
2023-03-10 13:01:47 +0300

    upd parser method names

--- app/lib/json_parser.rb
+++ app/lib/json_parser.rb
@@ -1,45 +1,43 @@
 # frozen_string_literal: true
 
 class JsonParser
-  class << self
-    def parse_javascript(data)
-      return [] if data.empty?
+  def self.parse_javascript(data)
+    return [] if data.empty?
 
-      JSON.parse(data).map do |check_data|
-        file_path = check_data['filePath']
-        errors = parse_javascript_errors(check_data['messages'])
-        { file_path:, errors: }
-      end
+    JSON.parse(data).map do |check_data|
+      file_path = check_data['filePath']
+      format_javascript_errors(check_data['messages'], file_path)
     end
+  end
 
-    def parse_ruby(data)
-      return [] if data.empty?
+  def self.parse_ruby(data)
+    return [] if data.empty?
 
-      JSON.parse(data)['files'].filter { |d| d.offences.any? }.map do |check_data|
-        file_path = check_data['path']
-        errors = parse_ruby_errors(check_data['offences'])
-        { file_path:, errors: }
-      end
+    JSON.parse(data)['files'].filter { |d| d.offences.any? }.map do |check_data|
+      file_path = check_data['path']
+      format_ruby_errors(check_data['offences'], file_path)
     end
+  end
 
-    def parse_javascript_errors(error_messages)
-      error_messages.map do |message|
-        {
-          message: message['message'],
-          rule: message['ruleId'],
-          line_column: "#{message['line']} : #{message['column']}"
-        }
-      end
+  private_class_method def self.format_javascript_errors(error_messages, file_path)
+    error_messages.map do |message|
+      {
+        file_path:,
+        message: message['message'],
+        rule: message['ruleId'],
+        location: "#{message['line']} : #{message['column']}"
+      }
     end
+  end
 
-    def parse_ruby_errors(error_messages)
-      error_messages.map do |message|
-        {
-          message: message['message'],
-          rule: message['cop_name'],
-          line_column: "#{message['location']['line']} : #{message['location']['column']}"
-        }
-      end
+  private_class_method def self.format_ruby_errors(error_messages, file_path)
+    error_messages.map do |message|
+      {
+        file_path:,
+        message: message['message'],
+        rule: message['cop_name'],
+        location: "#{message['location']['line']} : #{message['location']['column']}"
+      }
     end
   end
 end
