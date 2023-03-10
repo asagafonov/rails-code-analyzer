3997fd167777c15a8fd6c5423d01ce2b54cfa9fa
asagafonov <agafonov@buroburo.ru>
2023-03-10 18:15:19 +0300

    fix link format

--- app/jobs/fetch_last_commit_job.rb
+++ app/jobs/fetch_last_commit_job.rb
@@ -5,19 +5,18 @@ class FetchLastCommitJob < ApplicationJob
 
   def perform(check_id)
     repository_check = Repository::Check.find(check_id)
+
     repository_name = repository_check.repository.github
+    token = repository_check.repository.user.token
 
-    client = Octokit::Client.new(access_token: current_user&.token, auto_paginate: true)
+    client = Octokit::Client.new(
+      access_token: token,
+      auto_paginate: true
+    )
 
-    result = client.commits(full_link(repository_name)).first
+    result = client.commits(repository_name).first
     last_commit = result[:sha][..6]
 
     repository_check.update!(commit_id: last_commit)
-  end
-
-  private
-
-  def full_link(url)
-    "https://github.com/#{url}"
   end
 end
