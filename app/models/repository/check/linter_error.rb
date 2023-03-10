f77a6c680d4f56010f17b08c5655a760d1897284
asagafonov <agafonov@buroburo.ru>
2023-03-10 18:14:24 +0300

    add name for counter_cache column

--- app/models/repository/check/linter_error.rb
+++ app/models/repository/check/linter_error.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Repository::Check::LinterError < ApplicationRecord
-  belongs_to :check, counter_cache: true
+  belongs_to :check, counter_cache: :linter_errors_count
 end
