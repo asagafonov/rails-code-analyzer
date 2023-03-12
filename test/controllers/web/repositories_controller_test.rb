6d7735accc9ee27d62816a3e76ba7a6465cf01eb
asagafonov <a.s.agafonov@yandex.ru>
2023-03-12 11:00:36 +0300

    remove old tests

--- test/controllers/web/repositories_controller_test.rb
+++ test/controllers/web/repositories_controller_test.rb
@@ -23,26 +23,6 @@ class Web::RepositoriesControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_path
   end
 
-  test 'should get repositories list at new' do
-    uri_template = Addressable::Template.new 'https://api.github.com/user/repos?per_page=100'
-
-    response = load_fixture('files/repositories.json')
-
-    stub_request(:get, uri_template)
-      .to_return(
-        status: 200,
-        body: response,
-        headers: { 'Content-Type' => 'application/json' }
-      )
-
-    get new_repository_path
-
-    json = JSON.parse(response)
-
-    assert_response :success
-    assert { json&.length&.positive? }
-  end
-
   test 'unauthorized user should not get new' do
     sign_out
 
