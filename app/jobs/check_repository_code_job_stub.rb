705489060475a850b29ea7bbe28ae1f503bd0816
asagafonov <a.s.agafonov@yandex.ru>
2023-03-13 09:25:33 +0300

    add check repo job stub

---
+++ app/jobs/check_repository_code_job_stub.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class CheckRepositoryCodeJobStub < ApplicationJob
+  queue_as :default
+
+  def perform(check_id)
+    repository_check = Repository::Check.find(check_id)
+
+    20.times do
+      linter_error = repository_check.linter_errors.build(
+        file_path: [
+          'app',
+          'helpers/modal',
+          'components/Data'
+        ].sample,
+        message: Faker::Lorem.sentence(word_count: 3),
+        rule: Faker::Lorem.sentence(word_count: 1),
+        location: "#{[10, 15, 20].sample}:#{[3, 4, 12].sample}"
+      )
+
+      linter_error.save
+    end
+
+    repository_check.mark_as_failed!
+  end
+end
