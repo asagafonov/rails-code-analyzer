fff3c8d6c48ee1359b08fc7cfb604a444c7d1c20
asagafonov <a.s.agafonov@yandex.ru>
2023-03-13 15:27:42 +0300

    remove logs

--- app/jobs/fetch_last_commit_job.rb
+++ app/jobs/fetch_last_commit_job.rb
@@ -8,8 +8,6 @@ class FetchLastCommitJob < ApplicationJob
 
     commit_data = github_api(check.repository.user).fetch_last_commit_data(check)
 
-    puts '@commit'
-    pp commit_data
     check.update(
       last_commit_sha: commit_data[:last_commit_sha],
       last_commit_url: commit_data[:last_commit_url]
