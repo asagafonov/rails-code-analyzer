ae270b3c2347313f3643eea481e9f31c30ac4a3c
asagafonov <a.s.agafonov@yandex.ru>
2023-03-14 13:25:37 +0300

    upd logs

--- app/jobs/update_repository_job.rb
+++ app/jobs/update_repository_job.rb
@@ -11,8 +11,6 @@ class UpdateRepositoryJob < ApplicationJob
     api = github_api(repository.user)
 
     data = api.fetch_repository_data(repository)
-    puts '@data'
-    pp data
     api.create_hook(repository.github_id)
 
     repository.update(
