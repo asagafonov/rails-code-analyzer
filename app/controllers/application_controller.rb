530e5d82369196f38447f3517410f35de4632e57
asagafonov <a.s.agafonov@yandex.ru>
2023-03-17 12:50:11 +0300

    move pundit auth from global app controller to web

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include Pundit::Authorization
-
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
-  private
-
-  def user_not_authorized(exception)
-    policy_name = exception.policy.class.to_s.underscore
-    redirect_to root_path, alert: t("#{policy_name}.#{exception.query}", scope: 'pundit', default: :default)
-  end
 end
--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class Web::ApplicationController < ApplicationController
+  include Pundit::Authorization
   include AuthConcern
   helper_method :current_user
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized(exception)
+    policy_name = exception.policy.class.to_s.underscore
+    redirect_to root_path, alert: t("#{policy_name}.#{exception.query}", scope: 'pundit', default: :default)
+  end
 end
