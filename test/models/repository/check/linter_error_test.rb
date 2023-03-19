033818d1996a8de163f312685b510a79889b22ac
asagafonov <a.s.agafonov@yandex.ru>
2023-03-19 09:08:13 +0300

    remove unused test

--- test/models/repository/check/linter_error_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class Repository::Check::LinterErrorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
